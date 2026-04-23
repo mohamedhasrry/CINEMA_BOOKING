a6887727282ba6ecd1c4580ac8d50285d3cf6bbb
MOHAMED HARRIS. A <121000138+mohamedhasrry@users.noreply.github.com>
2026-04-23 11:19:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+// Add header shadow on scroll
+window.addEventListener('scroll', () => {
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+// Toggle menu icon & navbar
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
+// Close navbar on scroll
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+};
+
+// Home swiper
+var swiper = new Swiper('.home', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+// Coming soon swiper
+var swiper = new Swiper('.coming-container', {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 55000,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    0: { slidesPerView: 2 },
+    568: { slidesPerView: 3 },
+    768: { slidesPerView: 4 },
+    968: { slidesPerView: 5 },
+  },
+});
+
